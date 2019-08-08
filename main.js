f63335b7c0255c1de4bbd3b4bc4c3ef483b6a440
Matt Thornberry <mtberry16@hotmail.com>
2019-08-08 13:38:07 -0600

    added function stubs, document ready, strict mode

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+// User Visits Site
+
+// User Enters state to search for park
+// User submits entry
+function submitQuery() {}
+
+// Format query
+function createQueryEndpoint() {}
+
+//Fetch data from NPS API
+function fetchData() {}
+
+// Display results to DOM
+function displayResults() {}
+
+// Document ready
+$(document).ready(function() {
+  console.log("App ready, waiting for query!");
+  submitQuery();
+});
