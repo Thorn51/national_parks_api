b89151da59392a2d4460aaf509dbc1d16426cc9f
Matt Thornberry <mtberry16@hotmail.com>
2019-08-08 19:37:29 -0600

    started on scripts, added basic css

--- main.js
+++ main.js
@@ -3,13 +3,23 @@
 
 // User Enters state to search for park
 // User submits entry
-function submitQuery() {}
+function submitQuery() {
+    $query = 
+}
 
 // Format query
 function createQueryEndpoint() {}
 
 //Fetch data from NPS API
-function fetchData() {}
+function fetchData(query, maxResults = 10) {
+  const params = {
+    API_KEY: npsKey.my_key,
+    q: query,
+    limit: maxResults
+  };
+
+  const npsBaseUrl = "https://developer.nps.gov/api/v1";
+}
 
 // Display results to DOM
 function displayResults() {}
